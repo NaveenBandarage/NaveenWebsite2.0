9be4d2458335a0510d31d482f2435a22742ebb23
Naveen Bandarage <bandaragenaveen@gmail.com>
2019-12-20 23:04:30 +1300

    got [rojects to be inputted doesnt display

--- src/App.js
+++ src/App.js
@@ -8,25 +8,13 @@ import ImageGrid from './imagegrid'
 import './imagegrid.css'
 import SplashScreen from './splashscreen'
 import ContactMe from './contactMe'
-import Projects from './projects'
+import Projects from './api/projects'
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { Component } from 'react';
+
 
 // import FooterPart from './footersection/footer';
-class App extends Component{
-  state = { projects: [] };
 
-  // On load
-  componentDidMount() {
-      this.getProjects();
-  }
-  
-  // Set state with our projects
-  getProjects = () => {
-      fetch('/api/projects')
-          .then(res => res.json())
-          .then(projects => this.setState({projects}));
-  }
-}
 class Section extends React.Component {
 
   constructor(props) {
@@ -87,34 +75,113 @@ class Section extends React.Component {
 
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <NavBar/>
-      <SplashScreen/>
-        <Aboutme/>
-        <Projects/>
-    <ImageGrid/>
-    <ContactMe/>
-    {/* <FooterPart/>  for some reason this doesn't work properly*/}
-    {/* <a onClick={scrollToTop}>To the top!</a> */}
+class App extends Component {
+  // Initialize state
+  state = { projects: [] };
 
-    </div>
-  );
+  // On load
+  componentDidMount() {
+      this.getProjects();
+  }
+
+  // Set state with our projects
+  getProjects = () => {
+      fetch('api/projects')
+          .then(res => res.json())
+          .then(projects => this.setState({projects}));
+  }
+
+  render() {
+      const { projects } = this.state;
+
+      return (
+          <div className="App">
+            <NavBar/>
+              <SplashScreen/>
+              <Aboutme/>
+         <ImageGrid/>
+             <ContactMe/>
+            
+              {
+                  projects.length ? (
+                      projects.map((project, index) => (
+                          <div key={project.name}>
+                              <p><b><a href={project.html_url}>{project.name}</a></b></p>
+                              <p>{project.description}</p>
+                          </div>
+                      ))
+                  ) : (
+                      <div>
+                          I don't have any projects
+                      </div>
+                  )
+              }
+
+
+          </div>
+      );
+  }
 }
+//               
+//               <Projects/>
+
+// class App extends Component {
+//   // Initialize state
+//   state = { projects: [] };
+
+//   // On load
+//   componentDidMount() {
+//       this.getProjects();
+//   }
+
+//   // Set state with our projects
+//   getProjects = () => {
+//       fetch('api/projects.js')
+//           .then(res => res.json())
+//           .then(projects => this.setState({projects}));
+//   }
+
+//   render() {
+//       const { projects } = this.state;
+
+//       return (
+//           <div className="App">
+//             <header className="App-header">
+//               <img src={logo} className="App-logo" alt="logo" />
+//               <p>
+//                 Edit <code>src/App.js</code> and save to reload.
+//               </p>
+//               <a
+//                 className="App-link"
+//                 href="https://reactjs.org"
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//               >
+//                 Learn React
+//               </a>
+//             </header>
+//             
+//           {/* <FooterPart/>  for some reason this doesn't work properly*/}
+//           {/* <a onClick={scrollToTop}>To the top!</a> */}
+//           {
+//                   projects.length ? (
+//                       projects.map((project, index) => (
+//                           <div key={project.name}>
+//                               <p><b><a href={project.html_url}>{project.name}</a></b></p>
+//                               <p>{project.description}</p>
+//                           </div>
+//                       ))
+//                   ) : (
+//                       <div>
+//                           I don't have any projects
+//                       </div>
+//                   )
+//               }
+
+//           </div>
+//                       );
+
+//   }
+// }
 
 export default App;
