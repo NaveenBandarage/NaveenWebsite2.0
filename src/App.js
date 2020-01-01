321788463aa82ab1bf910f7a964a49f440d966e7
Naveen Bandarage <bandaragenaveen@gmail.com>
2020-01-01 16:06:16 +1300

    finally got the github projects to load up.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Aboutme from './aboutme'
@@ -8,72 +8,9 @@ import ImageGrid from './imagegrid'
 import './imagegrid.css'
 import SplashScreen from './splashscreen'
 import ContactMe from './contactMe'
-import Projects from './api/projects'
-import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
-import { Component } from 'react';
 
 
 // import FooterPart from './footersection/footer';
-
-class Section extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.scrollToTop = this.scrollToTop.bind(this);
-  }
-
-  componentDidMount() {
-
-    Events.scrollEvent.register('begin', function () {
-      console.log("begin", arguments);
-    });
-
-    Events.scrollEvent.register('end', function () {
-      console.log("end", arguments);
-    });
-
-  }
-  scrollToTop() {
-    scroll.scrollToTop();
-  }
-  scrollTo() {
-    scroller.scrollTo('scroll-to-element', {
-      duration: 800,
-      delay: 0,
-      smooth: 'easeInOutQuart'
-    })
-  }
-  scrollToWithContainer() {
-
-    let goToContainer = new Promise((resolve, reject) => {
-
-      Events.scrollEvent.register('end', () => {
-        resolve();
-        Events.scrollEvent.remove('end');
-      });
-
-      scroller.scrollTo('scroll-container', {
-        duration: 800,
-        delay: 0,
-        smooth: 'easeInOutQuart'
-      });
-
-    });
-
-    goToContainer.then(() =>
-      scroller.scrollTo('scroll-container-second-element', {
-        duration: 800,
-        delay: 0,
-        smooth: 'easeInOutQuart',
-        containerId: 'scroll-container'
-      }));
-  }
-  componentWillUnmount() {
-    Events.scrollEvent.remove('begin');
-    Events.scrollEvent.remove('end');
-  }
-
-}
 
 class App extends Component {
   // Initialize state
@@ -86,9 +23,9 @@ class App extends Component {
 
   // Set state with our projects
   getProjects = () => {
-      fetch('api/projects')
-          .then(res => res.json())
-          .then(projects => this.setState({projects}));
+    fetch('/api/projects')
+        .then(res => res.json())
+        .then(projects => this.setState({projects}));
   }
 
   render() {
@@ -182,6 +119,53 @@ class App extends Component {
 //                       );
 
 //   }
+// }
+// import './App.css';
+
+// class App extends Component {
+//     // Initialize state
+//     state = { projects: [] };
+
+//     // On load
+//     componentDidMount() {
+//         this.getProjects();
+//     }
+
+//     // Set state with our projects
+//     getProjects = () => {
+//         fetch('/api/projects')
+//             .then(res => res.json())
+//             .then(projects => this.setState({projects}));
+//     }
+
+//     render() {
+//         const { projects } = this.state;
+
+//         return (
+//             <div className="App">
+
+//                 <h1>Hi, my name is TITS</h1>
+//                 <h3>I'm a developer</h3>
+
+//                 <h4>Here are a few of my projects</h4>
+
+//                 {
+//                     projects.length ? (
+//                         projects.map((project, index) => (
+//                             <div key={project.name}>
+//                                 <p><b><a href={project.html_url}>{project.name}</a></b></p>
+//                                 <p>{project.description}</p>
+//                             </div>
+//                         ))
+//                     ) : (
+//                         <div>
+//                             I don't have any projects
+//                         </div>
+//                     )
+//                 }
+//             </div>
+//         );
+//     }
 // }
 
 export default App;
