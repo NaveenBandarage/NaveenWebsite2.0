3ce37fe3298585592c3d845258bc4e7370176ec6
NaveenBandarage <thebanterage@gmail.com>
2020-02-10 09:20:41 +1300

    adding text pretty sure.

--- src/imagegrid.js
+++ src/imagegrid.js
@@ -4,6 +4,7 @@ import image1 from './image1.JPG'
 import image2 from './image2.JPG'
 import image3 from './image4.JPG'
 
+
 const imageGrid = () =>{
 
 
@@ -15,11 +16,15 @@ const imageGrid = () =>{
       <p>These are the things I'm I am currently heavily invested in doing these things when I'm not coding:</p>
       <p>Feel free to explore them</p>
       <h2>Film Photography</h2> 
+      <p>The camera I primarily use to take photos is the <a href="https://camerapedia.fandom.com/wiki/Olympus_OM-101_/_OM-88" target = "_blank">Olympus OM-101</a>. However I have been playing around with a Kodak disposable camera.</p>
       {/* Got to make a seperate page for these photos*/}
       <h2>Anime</h2>
+      <p>I love anime and manga. I'm a huge shonen weeb. The series as of time of writing that I'm invested are My Hero Academia, Haikyu, One piece, Demon Slayer, Dr. Stone, Fire Force and (insert more here if think) : </p>
       {/* Make ap page for all the animes I'm watching */}
       <h2>Digital Photography and Videography</h2>
+      <p>The camera that I have been using is the Canon m50 with a 14-55 Lens.</p>
       <h2>Reading</h2>
+      
       {/* Have a page tracking all the books i've read so far. */}
 {/* 
   <div class="column">
